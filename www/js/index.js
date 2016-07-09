4eac9e0cca470706761879e495203e0519377a69
arijitupadhyay <arijitupadhyay@gmail.com>
2016-07-09 16:52:21 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,8 +33,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        window.location.href="http://phonexCFS.in";
-        //app.receivedEvent('deviceready');
+        var url = "http://phonegap.com"
+        window.open(url)
+        app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
